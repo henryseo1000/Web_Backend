be041580ca612d3bdca7e5b8ff1b6eebc0ab2fc6
서호준 <henryseo1000@gmail.com>
2023-12-31 00:33:27 +0900

    프로젝트 A 완료

---
+++ Project_A/src/main/java/org/edwith/webbe/cardmanager/dao/BusinessCardManagerDao.java
@@ -0,0 +1,83 @@
+package org.edwith.webbe.cardmanager.dao;
+
+import org.edwith.webbe.cardmanager.dto.BusinessCard;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class BusinessCardManagerDao {
+    String url = "jdbc:mysql://localhost/ddl";
+    String user = "root";
+    String password = "";
+
+    public List<BusinessCard> searchBusinessCard(String keyword){
+	    // 구현하세요.
+        List<BusinessCard> card_list = new ArrayList<>();
+        Connection conn = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+
+            String sql = "select * from info where name like ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, "%" + keyword + "%");
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String tel = rs.getString("tel");
+                String company = rs.getString("company");
+                Date date = rs.getDate("date");
+
+                BusinessCard role = new BusinessCard(name, tel, company);
+                role.setCreateDate(date);
+
+                card_list.add(role); // list에 반복할때마다 Role인스턴스를 생성하여 list에 추가한다.
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return card_list;
+    }
+
+    public BusinessCard addBusinessCard(BusinessCard businessCard){
+	    // 구현하세요.
+        Connection conn = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+
+            String name = businessCard.getName();
+            String tel = businessCard.getPhone();
+            String company = businessCard.getCompanyName();
+            Date date = businessCard.getCreateDate();
+
+            java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+
+            String sql = "insert into info(name, tel, company, date) values(?, ?, ?, ?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, name);
+            ps.setString(2, tel);
+            ps.setString(3, company);
+            ps.setDate(4, sqlDate);
+
+            ps.executeUpdate();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return businessCard;
+    }
+}
--- backend/target/backend/jdbcExam1.java
+++
@@ -1,9 +0,0 @@
-
-public class jdbcExam1 {
-	public static void main(String[] args) {
-		RoleDao dao = new RoleDao();
-		Role role = dao.getRole(100);
-		System.out.println(role);
-	}
-
-}
